1db349c14205c32b34589f7a929d2264cd709c60
uib08758 <uib08758@OZL2988W.cw01.contiwan.com>
2020-06-20 23:19:23 +0530

    Generic Example with wild card

--- Java8/src/com/generics/example/GenericExampleTwo.java
+++ Java8/src/com/generics/example/GenericExampleTwo.java
@@ -2,6 +2,7 @@ package com.generics.example;
 
 import java.io.InputStream;
 import java.io.Serializable;
+import java.util.Collection;
 
 public class GenericExampleTwo implements GenericInterfaceTypes<String, Boolean> {
 
@@ -18,6 +19,12 @@ public class GenericExampleTwo implements GenericInterfaceTypes<String, Boolean>
 
 	@Override
 	public <T extends InputStream & Serializable> void display(Boolean data, T Data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <T extends InputStream & Serializable> void display123(Collection<?> datas) {
 		// TODO Auto-generated method stub
 		
 	}
--- Java8/src/com/generics/example/GenericInterfaceTypes.java
+++ Java8/src/com/generics/example/GenericInterfaceTypes.java
@@ -2,6 +2,7 @@ package com.generics.example;
 
 import java.io.InputStream;
 import java.io.Serializable;
+import java.util.Collection;
 
 public interface GenericInterfaceTypes<T, R> {
 
@@ -10,4 +11,5 @@ public interface GenericInterfaceTypes<T, R> {
 	<T,R> R display();
 	
 	<T extends InputStream & Serializable>  void display(R data,T Data);
+	<T extends InputStream & Serializable>  void display123(Collection<?> datas);
 }
